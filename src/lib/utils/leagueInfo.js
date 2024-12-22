4f435889ffee44e71f85b998c0d8ec1116e33b06
Ally <ally.bhoy2@gmail.com>
2024-12-22 19:21:26 +0000

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -184,7 +184,7 @@ export const managers = [
      // "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "somewhere", // (optional)
       "bio": "impsum",
-      "photo": "/managers/paul.png", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/paul.PNG", // square ratio recommended (no larger than 500x500)
     //  "fantasyStart": 2014, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "ne", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -274,7 +274,7 @@ export const managers = [
       "bio": "impsum",
       "photo": "/managers/duncan.png", // square ratio recommended (no larger than 500x500)
      // "fantasyStart": 2019, // (optional) when did the manager start playing fantasy football
-      "favoriteTeam": "gb", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
+      "favoriteTeam": "min", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Ally", // Can be anything (usually your rival's name)
