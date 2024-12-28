9299e73c78692929375b0002ede534e7ed9752c7
Ally <ally.bhoy2@gmail.com>
2024-12-28 16:14:24 +0000

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -143,77 +143,77 @@
         <h4 class="noUnderscore clickable" on:click={() => goToSection(sixThree)}>6.3 Replacement Incentive</h4>
     <hr />
     
-    <h2 class="sectionHeading" bind:this={one}>Section 1 League Overview</h2>
-    <div class="subBlock">
-        <p>Welcome to EFFL, the Earlston Fantasy Football League.</p>
-        <p>This is a 12-team league formed in 2013 in the <a href="http://www.redlionearlston.co.uk/">Red Lion Pub</a> (Earlston) by Chris Powell and Alastair Reid.</p>
-        <p>The league is designed to be fun with the winner receiving the Earlston Fantasy Football trophy.</p>
-
-        <p>The league is designed to be a competition between owners, but it not a cut-throat league where anything goes.</p>
-        <p>The rules described below are designed to act as a guideline for overall league play, and any disputes will be handled by the executive committee of the league.</p>
-        <p>Please remember that the overall goal of this league is to have fun and enjoy the game of football.</p>
-        <p>If you have any question or concerns, please do not hesitate to ask.</p>
-    </div>
-
-    <h3 class="subSectionHeading" bind:this={oneOne}>1.1 Executive Committee</h3>
-    <div class="subBlock">
-        <p>The executive committee for EFFL consists of:</p>
-            <ul>
- 	            <li>Commissioner Alastair Reid</li>
- 	            <li>Co-Commissioner Chris Powell</li>
-            </ul>
-        <p>The executive committee will be responsible for resolving all disputes that are not expressly covered by this constitution.</p>
-        <p>If a committee member is involved in the dispute, that member will excuse themselves from the decision process.</p>
-        <p>While all owners are encouraged to submit suggestions to the committee, the committee's decision on all matters is final.</p>
-        <p>The executive committee will also have access to independent arbitration from external fantasy football commissioners known to the commissioner.</p>
-    </div>
-
-    <h3 class="subSectionHeading" bind:this={oneTwo}>1.2 Living Constitution</h3>
-    <div class="subBlock">
-        <p>It is important to note that this is a living, breathing constitution that will change and evolve over the lifetime of this league.</p>
-        <p>Changes will happen between seasons as often as possible and will be effective from that point moving forward.</p>
-        <p>If you have suggestions for changes, please submit them to one of the executive committee members.</p>
-    </div>
-
-    <h2 class="sectionHeading" bind:this={Two}>Section 2 League Financing</h2>    
-    <div class="subBlock">
-        <p>The EFFL no longer charges league fees.</p>
-        <p>Previously these covered the cost of the league and generated a small prize fund.</p>
-    </div>
-    <h3 class="subSectionHeading" bind:this={twoOne}>2.1 League Fee</h3>
-    <h3 class="subSectionHeading" bind:this={twoTwo}>2.2 Prize Money</h3>
-
-    <h2 class="sectionHeading" bind:this={Three}>Section 3 League Setup and Competition</h2> 
-    <h3 class="subSectionHeading" bind:this={threeOne}>3.1 Playoff Rankings</h3>
-    <h3 class="subSectionHeading" bind:this={threeTwo}>3.2 Playoffs</h3>
-
-    <h2 class="sectionHeading" bind:this={Four}>Section 4 Rosters and Lineups</h2> 
-        <h3 class="subSectionHeading" bind:this={fourOne}>4.1 The Draft</h3>
-        <h3 class="subSectionHeading" bind:this={fourTwo}>4.2 Free Agency</h3>
-        <h3 class="subSectionHeading" bind:this={fourThree}>4.3 Trades</h3>
-            <h4 bind:this={fourThreeOne}>4.3.1 Trade Veto</h4>
-        <h3 class="subSectionHeading" bind:this={fourFour}>4.4 Injured Reserve</h3>
-        <h3 class="subSectionHeading" bind:this={fourFive}>4.5 Starting Lineups</h3>
-            <h4 bind:this={fourFiveOne}>4.5.1 Position Designation</h4>
-            <h4 bind:this={fourFiveTwo}>4.5.2 Substitutions</h4>
-        <h3 class="subSectionHeading" bind:this={fourSix}>4.6 Bye Week Penalties</h3>
-        <h3 class="subSectionHeading" bind:this={fourSeven}>4.7 Active Ownership Requirements</h3>
-        <h3 class="subSectionHeading" bind:this={fourEight}>4.8 Anti-Tank Rules</h3>
-        <h3 class="subSectionHeading" bind:this={fourNine}>4.9 Rescheduled Games</h3>
-    
-    <h2 class="sectionHeading" bind:this={Five}>Section 5 Scoring</h2> 
-        <h3 class="subSectionHeading" bind:this={fiveOne}>5.1 Points Total and Final Score</h3>
-   
-    <h2 class="sectionHeading" bind:this={six}>Section 6 Offseason</h2>
-    
-        <h3 bind:this={sixOne}>6.1 Removing Managers</h3>
-            <p>Managers who fail to adhere to the rules above and spirit of the league will be removed and replaced. League fees will be refunded, but time involved with the league will not be reimbursed under any circumstances.</p>
-    
-        <h3 bind:this={sixTwo}>6.2 Replacing Managers</h3>
-            <p>When a manager needs to be replaced, the commisiooners will try to find a suitable candidate with some connection to at least a portion of the existing managers. Priority will be placed on managers who are eager to take on a long term commitment and will be very active within the league.</p>
-    
-        <h3 bind:this={sixThree}>6.3 Replacement Incentive</h3>
-            <p>In the event that a manager chooses to quit the league or is removed by a comissioner, an incoming manager may be offered reduced or waived fees</p>
+//    <h2 class="sectionHeading" bind:this={one}>Section 1 League Overview</h2>
+//    <div class="subBlock">
+//        <p>Welcome to EFFL, the Earlston Fantasy Football League.</p>
+//        <p>This is a 12-team league formed in 2013 in the <a href="http://www.redlionearlston.co.uk/">Red Lion Pub</a> (Earlston) by Chris Powell and Alastair Reid.</p>
+//        <p>The league is designed to be fun with the winner receiving the Earlston Fantasy Football trophy.</p>
+//
+//        <p>The league is designed to be a competition between owners, but it not a cut-throat league where anything goes.</p>
+//        <p>The rules described below are designed to act as a guideline for overall league play, and any disputes will be handled by the executive committee of the league.</p>
+//        <p>Please remember that the overall goal of this league is to have fun and enjoy the game of football.</p>
+//        <p>If you have any question or concerns, please do not hesitate to ask.</p>
+//    </div>
+//
+//    <h3 class="subSectionHeading" bind:this={oneOne}>1.1 Executive Committee</h3>
+//    <div class="subBlock">
+//        <p>The executive committee for EFFL consists of:</p>
+//            <ul>
+// 	            <li>Commissioner Alastair Reid</li>
+// 	            <li>Co-Commissioner Chris Powell</li>
+//            </ul>
+//        <p>The executive committee will be responsible for resolving all disputes that are not expressly covered by this constitution.</p>
+//        <p>If a committee member is involved in the dispute, that member will excuse themselves from the decision process.</p>
+//        <p>While all owners are encouraged to submit suggestions to the committee, the committee's decision on all matters is final.</p>
+//        <p>The executive committee will also have access to independent arbitration from external fantasy football commissioners known to the commissioner.</p>
+//    </div>
+//
+//    <h3 class="subSectionHeading" bind:this={oneTwo}>1.2 Living Constitution</h3>
+//    <div class="subBlock">
+//        <p>It is important to note that this is a living, breathing constitution that will change and evolve over the lifetime of this league.</p>
+//        <p>Changes will happen between seasons as often as possible and will be effective from that point moving forward.</p>
+//        <p>If you have suggestions for changes, please submit them to one of the executive committee members.</p>
+//    </div>
+//
+//    <h2 class="sectionHeading" bind:this={Two}>Section 2 League Financing</h2>    
+//    <div class="subBlock">
+//        <p>The EFFL no longer charges league fees.</p>
+//        <p>Previously these covered the cost of the league and generated a small prize fund.</p>
+//    </div>
+//    <h3 class="subSectionHeading" bind:this={twoOne}>2.1 League Fee</h3>
+//    <h3 class="subSectionHeading" bind:this={twoTwo}>2.2 Prize Money</h3>
+//
+//    <h2 class="sectionHeading" bind:this={Three}>Section 3 League Setup and Competition</h2> 
+//    <h3 class="subSectionHeading" bind:this={threeOne}>3.1 Playoff Rankings</h3>
+//    <h3 class="subSectionHeading" bind:this={threeTwo}>3.2 Playoffs</h3>
+//
+//    <h2 class="sectionHeading" bind:this={Four}>Section 4 Rosters and Lineups</h2> 
+//        <h3 class="subSectionHeading" bind:this={fourOne}>4.1 The Draft</h3>
+//        <h3 class="subSectionHeading" bind:this={fourTwo}>4.2 Free Agency</h3>
+//        <h3 class="subSectionHeading" bind:this={fourThree}>4.3 Trades</h3>
+//            <h4 bind:this={fourThreeOne}>4.3.1 Trade Veto</h4>
+//        <h3 class="subSectionHeading" bind:this={fourFour}>4.4 Injured Reserve</h3>
+//        <h3 class="subSectionHeading" bind:this={fourFive}>4.5 Starting Lineups</h3>
+//            <h4 bind:this={fourFiveOne}>4.5.1 Position Designation</h4>
+//            <h4 bind:this={fourFiveTwo}>4.5.2 Substitutions</h4>
+//        <h3 class="subSectionHeading" bind:this={fourSix}>4.6 Bye Week Penalties</h3>
+//        <h3 class="subSectionHeading" bind:this={fourSeven}>4.7 Active Ownership Requirements</h3>
+//        <h3 class="subSectionHeading" bind:this={fourEight}>4.8 Anti-Tank Rules</h3>
+//        <h3 class="subSectionHeading" bind:this={fourNine}>4.9 Rescheduled Games</h3>
+//    
+//    <h2 class="sectionHeading" bind:this={Five}>Section 5 Scoring</h2> 
+//        <h3 class="subSectionHeading" bind:this={fiveOne}>5.1 Points Total and Final Score</h3>
+//   
+//    <h2 class="sectionHeading" bind:this={six}>Section 6 Offseason</h2>
+//    
+//        <h3 bind:this={sixOne}>6.1 Removing Managers</h3>
+//            <p>Managers who fail to adhere to the rules above and spirit of the league will be removed and replaced. League fees will be refunded, but time involved with the league will not be reimbursed under any circumstances.</p>
+//   
+//        <h3 bind:this={sixTwo}>6.2 Replacing Managers</h3>
+//            <p>When a manager needs to be replaced, the commisiooners will try to find a suitable candidate with some connection to at least a portion of the existing managers. Priority will be placed on managers who are eager to take on a long term commitment and will be very active within the league.</p>
+//    
+//        <h3 bind:this={sixThree}>6.3 Replacement Incentive</h3>
+//            <p>In the event that a manager chooses to quit the league or is removed by a comissioner, an incoming manager may be offered reduced or waived fees</p>
 
 
 
